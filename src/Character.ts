f2e193731a0df111db1c2694955c5cc1e6ec323c
Guilherme Garcia <garciaguig@gmail.com>
2023-01-18 18:33:02 -0300

    :sparkles: feat: Cria a classe Monster e refatora a Fighter (Req. 09)

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -65,12 +65,12 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter):void {
+  attack(enemy: SimpleFighter):void {
     enemy.receiveDamage(this._strength);
     console.log(`${enemy} received an attack!`);
   }
 
-  special?(enemy: Fighter):void {
+  special?(enemy: SimpleFighter):void {
     enemy.receiveDamage(this._strength * (getRandomInt(1, 3)));
     console.log(`${enemy} received an special attack!`);
   }
